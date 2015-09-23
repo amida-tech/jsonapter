67801bd11c669ec9890fd8ca014a8bd9fe7dfd7e
Jacob Sachs <jacob.s.sachs@gmail.com>
2015-09-23 18:39:45 -0400

    support boolean default values

---
+++ test/test-boolean-default.js
@@ -0,0 +1,58 @@
+"use strict";
+
+var chai = require('chai');
+
+var expect = chai.expect;
+
+describe('templates with boolean default values', function () {
+    var bbj2j = require('../index');
+    var j2j = bbj2j.instance();
+    
+    it('default: false', function () {
+        var template = {
+            content: {
+                id: {
+                    dataKey: 'id',
+                    default: 'unknown'
+                },
+                boolVal: {
+                    dataKey: 'boolVal',
+                    default: false
+                }
+            }
+        };
+        
+        var r = j2j.run(template, {
+            id: 'test',
+        });
+        
+        expect(r).to.deep.equal({
+            id: 'test',
+            boolVal: false
+        });
+    });
+    
+    it('default: true', function () {
+        var template = {
+            content: {
+                id: {
+                    dataKey: 'id',
+                    default: 'unknown'
+                },
+                boolVal: {
+                    dataKey: 'boolVal',
+                    default: true
+                }
+            }
+        };
+        
+        var r = j2j.run(template, {
+            id: 'test',
+        });
+        
+        expect(r).to.deep.equal({
+            id: 'test',
+            boolVal: true
+        });
+    });
+});
