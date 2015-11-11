edef4a9a79fb2e30f4147bdfea3d3547a38df1cc
Afsin Ustundag <afsin.ustundag@us.pwc.com>
2015-11-11 07:56:17 -0500

    additional assign test

--- test/test-assign.js
+++ test/test-assign.js
@@ -6,6 +6,7 @@ var json2json = require('../index');
 
 var case_0 = require('./test_cases/case-assign-0');
 var case_1 = require('./test_cases/case-assign-1');
+var case_2 = require('./test_cases/case-assign-2');
 
 var expect = chai.expect;
 
@@ -20,5 +21,10 @@ describe('assign', function () {
     it('case-assign-1: null returning', function () {
         var actual = engine.run(case_1.template, case_1.input);
         expect(actual).to.equal(case_1.expected);
+    });
+
+    it('case-assign-2: split', function () {
+        var actual = engine.run(case_2.template, case_2.input);
+        expect(actual).to.deep.equal(case_2.expected);
     });
 });
