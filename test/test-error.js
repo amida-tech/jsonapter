ea131cf2708112c75c3f02b034a7c451cb2004f7
gdev <Gautam.Dev@siriusxm.com>
2017-04-21 19:08:14 -0500

    Modified jsonapter.js added error handling.

--- test/test-error.js
+++ test/test-error.js
@@ -11,7 +11,7 @@ var j2j = bbj2j.instance();
 
 describe('example error', function () {
 
-    it('dataKey - error', function () {
+    it('More than one actionKeys cannot be present in the template', function () {
 
         var template = {
             content: {
@@ -29,4 +29,117 @@ describe('example error', function () {
             expect(err).to.exist;
         }
     });
+
+    it('arrayContent is an array and cannot be empty', function () {
+
+        var template = {
+            arrayContent: []
+        };
+
+        try {
+            var r = j2j.run(template, {});
+        } catch (err) {
+            console.log(err);
+            expect(err).to.exist;
+        }
+    });
+
+    it('assign is an array and cannot be empty', function () {
+
+        var template = {
+            assign: []
+        };
+
+        try {
+            var r = j2j.run(template, {});
+        } catch (err) {
+            console.log(err);
+            expect(err).to.exist;
+        }
+    });
+
+    it('content cannot be empty', function () {
+
+        var template = {
+            content: {}
+        };
+
+        try {
+            var r = j2j.run(template, {});
+        } catch (err) {
+            console.log(err);
+            expect(err).to.exist;
+        }
+    });
+
+    it('firstOf is an array and cannot be empty', function () {
+
+        var template = {
+            firstOf: {}
+        };
+
+        try {
+            var r = j2j.run(template, {});
+        } catch (err) {
+            console.log(err);
+            expect(err).to.exist;
+        }
+    });
+
+    it('content is an object and cannot be empty', function () {
+
+        var template = {
+            content: {}
+        };
+
+        try {
+            var r = j2j.run(template, {});
+        } catch (err) {
+            console.log(err);
+            expect(err).to.exist;
+        }
+    });
+
+    it('content is an object, not an array', function () {
+
+        var template = {
+            content: []
+        };
+
+        try {
+            var r = j2j.run(template, {});
+        } catch (err) {
+            console.log(err);
+            expect(err).to.exist;
+        }
+    });
+
+    it('template is an object, not an array', function () {
+
+        var template = {
+            template: []
+        };
+
+        try {
+            var r = j2j.run(template, {});
+        } catch (err) {
+            console.log(err);
+            expect(err).to.exist;
+        }
+    });
+
+    it('template is an object and cannot be empty', function () {
+
+        var template = {
+            template: {}
+        };
+
+        try {
+            var r = j2j.run(template, {});
+        } catch (err) {
+            console.log(err);
+            expect(err).to.exist;
+        }
+    });
+
 });
